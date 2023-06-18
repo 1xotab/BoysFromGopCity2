277e9f533f155f049aa3a523be1e1a739e795bd8
1xotab <ninetonine319@gmail.com>
2023-06-18 12:49:30 +0300

    Initial commit

---
+++ src/main/java/com/example/boysfromgopcity/BoysFromGopCityApplication.java
@@ -0,0 +1,13 @@
+package com.example.boysfromgopcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoysFromGopCityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoysFromGopCityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/boysfromgopcity/BoysFromGopCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boysfromgopcity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoysFromGopCityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
